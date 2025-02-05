cceae6e52f74d601e5daa102505a5ef88459f7aa
dec <decleggal@gmail.com>
2025-02-05 14:13:55 +0100

    Updated vehicles to use abstract

---
+++ src/main/java/agency/AbstractVehicle.java
@@ -0,0 +1,71 @@
+package agency;
+
+import util.TimeProvider;
+
+public abstract class AbstractVehicle implements Vehicle{
+
+    private String brand;
+    private String model;
+    private int productionYear;
+
+    /**
+     * Create a new vehicle.
+     * @param brand The brand of the vehicle.
+     * @param model The model of the vehicle.
+     * @param productionYear The production year of the vehicle.
+     * @throws IllegalArgumentException if the production year is not between 1900 and the current year.
+     */
+    public AbstractVehicle(String brand, String model, int productionYear) throws IllegalArgumentException {
+        if(productionYear < 1900 || productionYear > TimeProvider.currentYearValue()) {
+            throw new IllegalArgumentException("Production year must be between 1900 and the current year. " + productionYear + " is invalid.");
+        }
+        this.brand = brand;
+        this.model = model;
+        this.productionYear = productionYear;
+    }
+
+    /**
+     * Get the brand of the car.
+     * @return The brand of the car.
+     */
+    @Override
+    public String getBrand() {
+        return this.brand;
+    }
+
+    /**
+     * Get the model of the car.
+     * @return The model of the car.
+     */
+    @Override
+    public String getModel() {
+        return this.model;
+    }
+
+    /**
+     * Get the production year of the car.
+     * @return The production year of the car.
+     */
+    @Override
+    public int getProductionYear() {
+        return this.productionYear;
+    }
+
+    @Override
+    public double dailyRentalPrice() {
+        return 0;
+    }
+
+    /**
+     * Test if two cars are equal.
+     * @param obj The object to compare to.
+     * @return true if the objects are equal, false otherwise.
+     */
+    public boolean equals(Object obj) {
+        if(obj.getClass() == this.getClass()) {
+            AbstractVehicle other = (AbstractVehicle) obj;
+            return this.brand.equals(other.brand) && this.model.equals(other.model) && this.productionYear == other.productionYear;
+        }
+        return false;
+    }
+}
--- src/main/java/agency/Car.java
+++ src/main/java/agency/Car.java
@@ -2,11 +2,8 @@ package agency;
 
 import util.TimeProvider;
 
-public class Car implements Vehicle{
+public class Car extends AbstractVehicle{
 
-    private String brand;
-    private String model;
-    private int productionYear;
     private int numberOfSeats;
 
     /**
@@ -18,31 +15,13 @@ public class Car implements Vehicle{
      * @throws IllegalArgumentException if the production year is not between 1900 and the current year or if the number of seats is less than 1.
      */
     public Car(String brand, String model, int productionYear, int numberOfSeats) throws IllegalArgumentException {
-        if(productionYear < 1900 || productionYear > TimeProvider.currentYearValue()) {
-            throw new IllegalArgumentException("Production year must be between 1900 and the current year. " + productionYear + " is invalid.");
-        }
+        super(brand, model, productionYear);
 
         if(numberOfSeats < 1) {
             throw new IllegalArgumentException("Number of seats must be at least 1. " + numberOfSeats + " is invalid.");
         }
 
-        this.brand = brand;
-        this.model = model;
-        this.productionYear = productionYear;
         this.numberOfSeats = numberOfSeats;
-    }
-
-    /**
-     * Test if two cars are equal.
-     * @param obj The object to compare to.
-     * @return true if the objects are equal, false otherwise.
-     */
-    public boolean equals(Object obj) {
-        if(obj instanceof Car) {
-            Car other = (Car) obj;
-            return this.brand.equals(other.brand) && this.model.equals(other.model) && this.productionYear == other.productionYear;
-        }
-        return false;
     }
 
     /**
@@ -50,34 +29,7 @@ public class Car implements Vehicle{
      * @return true if the car is new (5 years old or less), false otherwise.
      */
     public boolean isNew(){
-        return this.productionYear - TimeProvider.currentYearValue() >= 5;
-    }
-
-    /**
-     * Get the brand of the car.
-     * @return The brand of the car.
-     */
-    @Override
-    public String getBrand() {
-        return this.brand;
-    }
-
-    /**
-     * Get the model of the car.
-     * @return The model of the car.
-     */
-    @Override
-    public String getModel() {
-        return this.model;
-    }
-
-    /**
-     * Get the production year of the car.
-     * @return The production year of the car.
-     */
-    @Override
-    public int getProductionYear() {
-        return this.productionYear;
+        return this.getProductionYear() - TimeProvider.currentYearValue() >= 5;
     }
 
     /**
@@ -99,9 +51,9 @@ public class Car implements Vehicle{
      */
     @Override
     public String toString() {
-        return "Car " + this.brand + " " +
-                this.model + " " +
-                this.productionYear + " (" +
+        return "Car " + this.getBrand() + " " +
+                this.getModel() + " " +
+                this.getProductionYear() + " (" +
                 this.numberOfSeats +
                 " seat" + (this.numberOfSeats > 1 ? "s" : "") +  ") : " +
                 dailyRentalPrice() + "€";
