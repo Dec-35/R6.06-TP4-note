da154177358eb9d551bbe3d3f24acd54660a54f8
dec <decleggal@gmail.com>
2025-02-05 15:28:11 +0100

    Finished car and motorbike tests

--- src/test/java/agency/AgencyTests.java
+++ src/test/java/agency/AgencyTests.java
@@ -28,8 +28,12 @@ public class AgencyTests {
         @DisplayName("Car creation test with invalid production year")
         public void carCreationTestWithInvalidProductionYear() {
             int invalidFutureYear = TimeProvider.currentYearValue() + 1;
-            assertThrows(IllegalArgumentException.class, () -> new Car("Toyota", "Corolla", invalidFutureYear, 4));
-            assertThrows(IllegalArgumentException.class, () -> new Car("Toyota", "Corolla", 1899, 4));
+            IllegalArgumentException exception1 = assertThrows(IllegalArgumentException.class, () -> new Car("Toyota", "Corolla", invalidFutureYear, 4));
+            IllegalArgumentException exception2 = assertThrows(IllegalArgumentException.class, () -> new Car("Toyota", "Corolla", 1899, 4));
+
+            assertEquals("Production year must be between 1900 and the current year. " + invalidFutureYear + " is invalid.", exception1.getMessage());
+            assertEquals("Production year must be between 1900 and the current year. 1899 is invalid.", exception2.getMessage());
+
         }
 
         @ParameterizedTest(name = "Car creation test with invalid number of seats: {0}")
@@ -40,7 +44,8 @@ public class AgencyTests {
                 "-10"
         })
         public void carCreationTestWithInvalidNumberOfSeats(int invalidNumberOfSeats) {
-            assertThrows(IllegalArgumentException.class, () -> new Car("Toyota", "Corolla", 2019, invalidNumberOfSeats));
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Car("Toyota", "Corolla", 2019, invalidNumberOfSeats));
+            assertEquals("Number of seats must be at least 1. " + invalidNumberOfSeats + " is invalid.", exception.getMessage());
         }
 
         @Test
@@ -48,6 +53,61 @@ public class AgencyTests {
         public void newCarPriceTest() {
             Car car = new Car("Toyota", "Corolla", 2024, 4);
             assertEquals(4*40, car.dailyRentalPrice());
+        }
+
+        @ParameterizedTest
+        @DisplayName("Car toString test")
+        @CsvSource({
+                "4, 'Car Toyota Corolla 2019 (4 seats) : 80.0€'",
+                "1, 'Car Toyota Corolla 2019 (1 seat) : 20.0€'"
+        })
+        public void carToStringTest(int numberOfSeats, String expectedString) {
+            Car car = new Car("Toyota", "Corolla", 2019, numberOfSeats);
+            assertEquals(expectedString, car.toString());
+        }
+
+    }
+
+    @DisplayName("Motorbike tests")
+    @Nested
+    class MotorbikeTests {
+        @Test
+        @DisplayName("Motorbike creation test")
+        public void motorbikeCreationTest() {
+            Motorbike motorbike = new Motorbike("Honda", "CBR", 2020, 500);
+            assertEquals("Honda", motorbike.getBrand());
+            assertEquals("CBR", motorbike.getModel());
+            assertEquals(2020, motorbike.getProductionYear());
+            assertEquals(500*0.25, motorbike.dailyRentalPrice());
+        }
+
+        @Test
+        @DisplayName("Motorbike creation test with invalid production year")
+        public void motorbikeCreationTestWithInvalidProductionYear() {
+            int invalidFutureYear = TimeProvider.currentYearValue() + 1;
+            IllegalArgumentException exception1 = assertThrows(IllegalArgumentException.class, () -> new Motorbike("Honda", "CBR", invalidFutureYear, 500));
+            IllegalArgumentException exception2 = assertThrows(IllegalArgumentException.class, () -> new Motorbike("Honda", "CBR", 1899, 500));
+
+            assertEquals("Production year must be between 1900 and the current year. " + invalidFutureYear + " is invalid.", exception1.getMessage());
+            assertEquals("Production year must be between 1900 and the current year. 1899 is invalid.", exception2.getMessage());
+        }
+
+        @ParameterizedTest(name = "Motorbike creation test with invalid cylinder capacity: {0}")
+        @DisplayName("Motorbike creation test with invalid cylinder capacity")
+        @CsvSource({
+                "49",
+                "-1",
+        })
+        public void motorbikeCreationTestWithInvalidCylinderCapacity(int invalidCylinderCapacity) {
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Motorbike("Honda", "CBR", 2020, invalidCylinderCapacity));
+            assertEquals("Cylinder capacity must be at least 50. " + invalidCylinderCapacity + " is invalid.", exception.getMessage());
+        }
+
+        @Test
+        @DisplayName("Motorbike toString test")
+        public void motorbikeToStringTest() {
+            Motorbike motorbike = new Motorbike("Honda", "CBR", 2020, 500);
+            assertEquals("Motorbike Honda CBR 2020 (500cm3) 125.0€", motorbike.toString());
         }
 
     }
