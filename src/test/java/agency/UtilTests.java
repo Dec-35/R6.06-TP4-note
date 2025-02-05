b3306d57b4e89b618d63d82ad27357071c179569
dec <decleggal@gmail.com>
2025-02-05 16:06:54 +0100

    Finished util tests

--- src/test/java/agency/UtilTests.java
+++ src/test/java/agency/UtilTests.java
@@ -1,7 +1,21 @@
 package agency;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import util.TimeProvider;
 
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Tag("util")
 @DisplayName("Util tests")
 public class UtilTests {
+    @Test
+    @DisplayName("Test current year value")
+    public void testCurrentYearValue() {
+        TimeProvider timeProvider = new TimeProvider();
+        assertEquals(timeProvider.currentYearValue(), new Date().getYear() + 1900);
+    }
 }
