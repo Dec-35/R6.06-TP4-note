32d9362fe2d75274a7f3472222a1d3ed7d4576e2
dec <decleggal@gmail.com>
2025-02-05 14:58:26 +0100

    Added rented vehicles

--- src/main/java/agency/RentalAgency.java
+++ src/main/java/agency/RentalAgency.java
@@ -1,10 +1,15 @@
 package agency;
 
+import agency.exceptions.UnknownVehicleException;
+
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 public class RentalAgency {
     private List<Vehicle> vehicles;
+    private Map<Client, Vehicle> rentedVehicles;
 
     /**
      * Create a new rental agency with a list of vehicles.
@@ -72,7 +77,6 @@ public class RentalAgency {
         return selectedVehicles;
     }
 
-
     /**
     * Prints the vehicles (one by line) of this agency that satisfy the specified criterion
     * @param criterion the criterion that the selected cars must satisfy
@@ -81,5 +85,62 @@ public class RentalAgency {
         for(Vehicle vehicle : this.select(criterion)){
             System.out.println(vehicle);
         }
+    }
+
+    /**
+     * Test if a vehicle is rented by a client.
+     * @param client The client to test.
+     * @return true if the client has a rented vehicle, false otherwise.
+     */
+    public boolean aVehicleIsRentedBy(Client client){
+        return this.rentedVehicles.containsKey(client);
+    }
+
+    /**
+     * Test if a vehicle is rented.
+     * @param vehicle The vehicle to test.
+     * @return true if the vehicle is rented, false otherwise.
+     */
+    public boolean vehicleIsRented(Vehicle vehicle){
+        return this.rentedVehicles.containsValue(vehicle);
+    }
+
+    /**
+     * Rent a vehicle to a client.
+     * @param client The client who wants to rent a vehicle.
+     * @param vehicle The vehicle to rent.
+     * @return The daily rental price of the vehicle.
+     * @throws UnknownVehicleException if the vehicle is not in the list.
+     * @throws IllegalStateException if the client already has a rented vehicle or if the vehicle is already rented.
+     */
+    public double rentVehicle(Client client, Vehicle vehicle) throws UnknownVehicleException, IllegalStateException {
+        if(!this.vehicles.contains(vehicle)){
+            throw new UnknownVehicleException(vehicle);
+        }
+        if(this.aVehicleIsRentedBy(client)){
+            throw new IllegalStateException("Client " + client + " already has a rented vehicle.");
+        }
+        if(this.vehicleIsRented(vehicle)){
+            throw new IllegalStateException("Vehicle " + vehicle + " is already rented.");
+        }
+
+        this.rentedVehicles.put(client, vehicle);
+        return vehicle.dailyRentalPrice();
+    }
+
+    /**
+     * Return a vehicle from a client.
+     * @param client The client who wants to return a vehicle.
+     */
+    public void returnVehicle(Client client){
+        this.rentedVehicles.remove(client);
+    }
+
+    /**
+     * Get the list of rented vehicles.
+     * @return The list of rented vehicles.
+     */
+    public Collection<Vehicle> allRentedVehicles(){
+        return this.rentedVehicles.values();
     }
 }
