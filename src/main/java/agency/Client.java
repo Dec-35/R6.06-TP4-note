c2b0a8e39d4fefd02ca5e76f976dc7721e4672f2
dec <decleggal@gmail.com>
2025-02-05 14:50:24 +0100

    Added client

---
+++ src/main/java/agency/Client.java
@@ -0,0 +1,31 @@
+package agency;
+
+public class Client {
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+}
